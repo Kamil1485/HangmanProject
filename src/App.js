2fcaa665a8e7f67ed643145da4c39302228d8878
Kamil1485 <81561240+Kamil1485@users.noreply.github.com>
2023-03-30 16:32:14 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,18 @@ import WrongLetters from "./componentler/WrongLetters";
 import Show from "./Uyarı/Show"
    //8-
   // const kelimeler=["kratos","thor","jenna","spartacus","gannıcus","joel"]
-   const kelimetahmin=[{ipucu:"God Of War Karakteri",kelime:"kratos"},{ipucu:"Days Gone Karakteri",kelime:"boozer"},{ipucu:"Last of Us Karakteri",kelime:"ellıe"},{ipucu:"Read Dead Redamption Karakteri",kelime:"arthur"},{ipucu:"Resident Evil Karakteri",kelime:"ada"}]
+   const kelimetahmin=[
+    {ipucu:"God Of War Karakteri",kelime:"kratos"},
+    {ipucu:"Days Gone Karakteri",kelime:"boozer"},
+    {ipucu:"Last of Us Karakteri",kelime:"ellıe"},
+    {ipucu:"Read Dead Redamption Karakteri",kelime:"arthur"},
+    {ipucu:"Resident Evil Karakteri",kelime:"ada"},
+    {ipucu:"Futbolda hem orta yuvarlak hem başlama vuruşu anlamlarına gelen söz",kelime:"santra"},
+    {ipucu:"Takım çalışmalarında ekip elemanlarının sorumluluk ve yükümlülükleri paylaşması",kelime:"ısbolumu"},
+    {ipucu:"Hastane, ev, fabrika gibi yerlerden kullanılmış ve kullanıcının artık işine yaramayan maddelerin tümü",kelime:"atık"},
+    {ipucu:"Hastane, ev, fabrika gibi yerlerden kullanılmış ve kullanıcının artık işine yaramayan maddelerin tümü",kelime:"panik"},
+  ]
+  
    //let secilenKelime=kelimeler[Math.floor(Math.random()*kelimeler.length)];
    
    //28-
@@ -27,7 +38,7 @@ const[hatalıHarfler,setHatalıHarfler]=useState([]);
 const[uyarıGoster,setUyarıGoster]=useState(false);
 
 //27-
-
+const[score,setScore]=useState(0);
 //14-
 useEffect(()=>{
   const tusaBasma=(event)=>{
@@ -38,21 +49,26 @@ useEffect(()=>{
         if(secilenKelime.includes(harf)){
             if(!dogruHarfler.includes(harf)){
               setDogruHarfler(curr=>[...curr,harf])
+              setScore(score+(secilenKelime.length)*10);
             }else{
             Show(setUyarıGoster);
             }
         }else{
             if(!hatalıHarfler.includes(harf)){
               setHatalıHarfler(curr=>[...curr,harf]);
+              setScore(score-(secilenKelime.length)*5);
             }else{
               Show(setUyarıGoster)
             }
-        }
+        }  
     }
 
   }
+  
   window.addEventListener("keydown",tusaBasma);
   return()=>window.removeEventListener("keydown",tusaBasma);
+  
+// eslint-disable-next-line react-hooks/exhaustive-deps
 },[dogruHarfler,hatalıHarfler,oyunOyna])
 //26-
 
@@ -66,19 +82,20 @@ function TekrarOyna(e) {
 
   secilenTahmin=kelimetahmin[Math.floor(Math.random()*kelimetahmin.length)];
   secilenKelime=secilenTahmin.kelime;
+  setScore(0);
 }
 
 return (
     
     <div>
-    <Header secilenTahim={secilenTahmin} hatalıHarfler={hatalıHarfler}/>
+    <Header secilenTahmin={secilenTahmin} hatalıHarfler={hatalıHarfler} score={score}/>
     <div className={`game-container ${oyunOyna}`}>
       {/*16 */}
       <Figure hatalıHarfler={hatalıHarfler}/>
       {/**12 */} 
       <WrongLetters hatalıHarfler={hatalıHarfler} />
       {/*10*/}
-      <Word secilenKelime={secilenKelime} dogruHarfler={dogruHarfler} />
+      <Word  secilenKelime={secilenKelime} dogruHarfler={dogruHarfler} setDogruHarfler={setDogruHarfler} setHatalıHarfler={setHatalıHarfler} hatalıHarfler={hatalıHarfler} setScore={setScore} score={score}/>
     </div>
     {/*21 */}
     <Popup dogruHarfler={dogruHarfler} hatalıHarfler={hatalıHarfler} secilenKelime={secilenKelime} setOyunOyna={setOyunOyna} TekrarOyna={TekrarOyna}/>
