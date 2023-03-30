74e1ebbad1aa3420d46a2bd400d2e49689eb7d6d
Kamil1485 <81561240+Kamil1485@users.noreply.github.com>
2023-03-30 16:45:47 +0300

    Update Header.js

--- src/componentler/Header.js
+++ src/componentler/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 const Header = ({secilenTahmin,hatalıHarfler,score}) => {
     console.log(secilenTahmin)
     return (
@@ -9,14 +8,6 @@ const Header = ({secilenTahmin,hatalıHarfler,score}) => {
             <h3>{secilenTahmin.ipucu} ?</h3>
             <p>Hata Sayısı:{hatalıHarfler.length}/6</p>
             <span>Skor:{score}</span>
-
-const Header = ({secilenTahmin,hatalıHarfler}) => {
-    //console.log(secilenTahim)
-    return (
-        <div>
-            <h1>Adam Asmaca Oyunu</h1>
-            <h3>{secilenTahmin?.ipucu} ?</h3>
-            <p>Hata Sayısı:{hatalıHarfler.length}/6</p>
         </div>
     );
 }
