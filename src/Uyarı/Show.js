3aa185d43294bb1fb490164741a89ebc685d8993
Kamil1485 <81561240+Kamil1485@users.noreply.github.com>
2023-03-30 16:41:54 +0300

    Update Show.js

--- src/Uyarı/Show.js
+++ src/Uyarı/Show.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
-//20 Show a setUyarıGoster metodunu gönderiyoruz uyarıgoster statini true yapıyoruz 2sn sonra false a döndürüyoruz.
-=======
+
 //20 Show a setUyarıGoster metodunu gönderiyoruz uyarıgoster statini true yapıyoruz uyarıyı gösteriyoruz,2sn sonra false a döndürüyoruz.
->>>>>>> c18f8fd631e9c6801a9dc0e70dfa6e0b12addf49
+
 const Show = (setUyarıGoster) => {
     setUyarıGoster(true);
     setTimeout(() => {
